2f65e75fefac4de4b37bd994e96a480dc958223d
FabianUlises <ulisesfab.car@outlook.com>
2023-04-05 14:14:50 -0700

    Fetching and receiving data from pokemon api

--- src/components/pokedex/Pokedex.js
+++ src/components/pokedex/Pokedex.js
@@ -1,11 +1,16 @@
 // Dependencies
 import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const Pokedex = () => {
   // Useeffect
   useEffect(() => {
     // test cl
     console.log('hello there');
+    // Fetching pokemon api
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=400')
+      .then(res => res.statusText === 'OK' ? console.log('success', res.data) : console.log('err', res))
+      .catch(err => console.log(err));
   }, []);
   return (
     <div>Pokedex</div>
