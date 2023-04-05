56ae5d439a3e979708179d33ca2c117f71931524
FabianUlises <ulisesfab.car@outlook.com>
2023-04-05 14:03:24 -0700

    Installed axios

--- src/components/pokedex/Pokedex.js
+++ src/components/pokedex/Pokedex.js
@@ -1,6 +1,12 @@
-import React from 'react';
+// Dependencies
+import React, { useEffect } from 'react';
 
 const Pokedex = () => {
+  // Useeffect
+  useEffect(() => {
+    // test cl
+    console.log('hello there');
+  }, []);
   return (
     <div>Pokedex</div>
   );
