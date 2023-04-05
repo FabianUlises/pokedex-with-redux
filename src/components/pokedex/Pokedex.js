989573d3e1a778f930d897673cfdbd24f922bc5d
FabianUlises <ulisesfab.car@outlook.com>
2023-04-05 14:28:25 -0700

    mapping through state data to dispay data on dom

--- src/components/pokedex/Pokedex.js
+++ src/components/pokedex/Pokedex.js
@@ -1,19 +1,40 @@
 // Dependencies
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Pokedex = () => {
+  // State
+  const [pokemon, setPokemon] = useState(null);
   // Useeffect
   useEffect(() => {
     // test cl
     console.log('hello there');
     // Fetching pokemon api
-    axios.get('https://pokeapi.co/api/v2/pokemon?limit=400')
-      .then(res => res.statusText === 'OK' ? console.log('success', res.data) : console.log('err', res))
-      .catch(err => console.log(err));
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=8')
+      .then(res => {
+        if(res.statusText === 'OK'){
+         const {results} = res.data;
+         let newPokemon = [];
+         results.forEach((item, i) => {
+          i++;
+          let pokemonObj = {
+            id: i,
+            name: item.name
+          };
+          newPokemon.push(pokemonObj);
+         })
+         setPokemon(newPokemon);
+        }
+      })
+
   }, []);
+  let displayPokemon = pokemon ? pokemon.map((item) => (
+    `${item.name}`
+  )) : 'LOADING ....';
   return (
-    <div>Pokedex</div>
+    <div>Pokedex
+      {displayPokemon}
+    </div>
   );
 };
 
