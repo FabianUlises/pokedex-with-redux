83a4c08a4aee4878a5957645f726030d63683b86
FabianUlises <ulisesfab.car@outlook.com>
2023-04-05 14:59:24 -0700

    Refactored code. Moved useEffect and useState from card component to game component and passed them as props

--- src/components/pokedex/Pokedex.js
+++ src/components/pokedex/Pokedex.js
@@ -1,34 +1,10 @@
 // Dependencies
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 // Components
 import PokeCard from '../pokecard/Pokecard';
-const Pokedex = () => {
-  // State
-  const [pokemon, setPokemon] = useState(null);
-  // Useeffect
-  useEffect(() => {
-    // test cl
-    console.log('hello there');
-    // Fetching pokemon api
-    axios.get('https://pokeapi.co/api/v2/pokemon?limit=8')
-      .then(res => {
-        if(res.statusText === 'OK'){
-         const {results} = res.data;
-         let newPokemon = [];
-         results.forEach((item, i) => {
-          i++;
-          let pokemonObj = {
-            id: i,
-            name: item.name
-          };
-          newPokemon.push(pokemonObj);
-         })
-         setPokemon(newPokemon);
-        }
-      })
-
-  }, []);
+const Pokedex = (props) => {
+// Destructurin from props
+const {pokemon} = props;
   // Mapping through state data to display pokemon
   let displayPokemon = pokemon ? pokemon.map((item) => (
     <PokeCard name={item.name} />
--- src/components/pokegame/Pokegame.js
+++ src/components/pokegame/Pokegame.js
@@ -1,12 +1,39 @@
 // Dependencies
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 // Components
 import Pokedex from './../pokedex/Pokedex';
 const Pokegame = () => {
+    // State
+    const [pokemon, setPokemon] = useState(null);
+    // Useeffect
+    useEffect(() => {
+    // test cl
+    console.log('hello there');
+    // Fetching pokemon api
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=8')
+        .then(res => {
+            if(res.statusText === 'OK'){
+                const {results} = res.data;
+                let newPokemon = [];
+                results.forEach((item, i) => {
+                    i++;
+                    let pokemonObj = {
+                    id: i,
+                    name: item.name
+                };
+                newPokemon.push(pokemonObj);
+        })
+        //  Updating state with data from api
+        setPokemon(newPokemon);
+        }
+      })
+
+  }, []);
   return (
     <div className='game-container'>
-        <Pokedex  />
-        <Pokedex  />
+        <Pokedex pokemon={pokemon} />
+        <Pokedex pokemon={pokemon} />
     </div>
   )
 }
