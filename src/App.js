e27d43848a46f761d4282cadc972e1c27ebbaf3a
FabianUlises <ulisesfab.car@outlook.com>
2023-04-05 13:55:19 -0700

    Modified some styles on nav component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // Components
 import Pokedex from './components/pokedex/Pokedex';
+import AppNav from './components/appnav/AppNav';
 function App() {
   return (
     <Router>
+      <AppNav />
       <Routes>
         <Route path='/' Component={Pokedex} />
       </Routes>
