ecba943c7e0ca60160a78676b00f8c5c4132275d
FabianUlises <ulisesfab.car@outlook.com>
2023-04-06 16:17:09 -0700

    Configuring redux store to app

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+// Dependencies
+import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import { storage } from "redux-persist/storage";
+// Reducer
+import rootReducer from './reducer';
+
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export let store = createStore(persistedReducer);
+export let persistor = persistStore(store);
